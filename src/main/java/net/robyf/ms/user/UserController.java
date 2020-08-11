07ec22984a5b2739bc1023650c7c65855542f50a
Roberto Fasciolo <rob@robyf.net>
2020-08-11 11:40:49 +0300

    More documentation stuff

--- src/main/java/net/robyf/ms/user/UserController.java
+++ src/main/java/net/robyf/ms/user/UserController.java
@@ -17,11 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zalando.problem.Problem;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -39,34 +41,34 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
-    public ResponseEntity<Iterable<User>> listUsers() {
+    @RequestMapping(method = RequestMethod.GET, path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> listUsers() {
         return ResponseEntity.ok(StreamSupport.stream(repository.findAll().spliterator(), false).map(it -> User.build(it)).collect(Collectors.toList()));
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/")
+    @RequestMapping(method = RequestMethod.POST, path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> createUser(@Valid @RequestBody final CreateUserRequest request) {
         log.info("Create user request {}", request);
         return ResponseEntity.ok(service.createUser(request));
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/id/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Retrieves a user by id")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Success", response = User.class),
-            @ApiResponse(code = 404, message = "Not found")
+            @ApiResponse(code = 404, message = "Not found", response = Problem.class)
     })
     public ResponseEntity<User> getUserById(@Valid @PathVariable final UUID id) {
         return ResponseEntity.ok(service.getUserById(id));
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/email/{email}")
+    @RequestMapping(method = RequestMethod.GET, path = "/email/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> getUserByEmail(@Valid @Email @PathVariable final String email) {
         return ResponseEntity.ok(service.getUserByEmail(email));
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/authenticate")
-    public ResponseEntity<AuthenticateResponse> createUser(@Valid @RequestBody final AuthenticateRequest request) {
+    @RequestMapping(method = RequestMethod.POST, path = "/authenticate", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AuthenticateResponse> authenticate(@Valid @RequestBody final AuthenticateRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
