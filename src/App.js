2ace559a64f488e9f70268b36de23a4f07021837
Roberto Fasciolo <rob@robyf.net>
2020-08-19 12:47:14 +0300

    Basic loading component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import {
 
 import { currentUser } from './api/user';
 import Routes from './Routes';
+import { Loading } from './components';
 
 function App() {
+
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     const fetchData = async () => {
       const user = await currentUser();
       console.log('Fetched user: ', user);
+      setLoading(false);
     };
     fetchData();
   }, []);
+
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
 
   return (
     <Router>
