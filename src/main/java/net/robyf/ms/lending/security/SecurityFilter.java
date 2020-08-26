7b18173480d0374054c19f839e47a3cf611d6720
Roberto Fasciolo <rob@robyf.net>
2020-08-26 12:05:07 +0300

    Parsing and validating the jwt, not doing anything with it yet

---
+++ src/main/java/net/robyf/ms/lending/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package net.robyf.ms.lending.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Order(1)
+@Slf4j
+public class SecurityFilter implements Filter {
+
+    // TODO: use RS256 or RS512
+    private Algorithm algorithm = Algorithm.HMAC256("_secret_password_");
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String authHeader = httpRequest.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwtString = authHeader.substring(7);
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("frontend-service").build();
+            try {
+                DecodedJWT jwt = verifier.verify(jwtString);
+                String userIdStr = jwt.getClaim("user_id").asString();
+                String accountIdStr = jwt.getClaim("account_id").asString();
+                log.info("Incoming request for user {} account {}", userIdStr, accountIdStr);
+            } catch (JWTVerificationException jve) {
+                log.error("Error verifying jwt", jve);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
