037c4e4334f15bf571bb8d8346b1b6415851aff1
Roberto Fasciolo <rob@robyf.net>
2020-08-28 12:07:47 +0300

    Using autoconfiguration

--- src/main/java/net/robyf/ms/lending/Application.java
+++ src/main/java/net/robyf/ms/lending/Application.java
@@ -1,50 +1,18 @@
 package net.robyf.ms.lending;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import net.robyf.ms.autoconfigure.EnableMicroserviceAutoconfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.zalando.problem.ProblemModule;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableCircuitBreaker
 @EnableFeignClients
+@EnableMicroserviceAutoconfiguration
 public class Application {
-
-    @Bean
-    @Primary
-    public ObjectMapper objectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper().registerModule(new ProblemModule().withStackTraces());
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        module.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer());
-        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        module.addDeserializer(ZonedDateTime.class, new ZonedDateTimeDeserializer());
-        objectMapper.registerModule(module);
-
-        return objectMapper;
-    }
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
-    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
