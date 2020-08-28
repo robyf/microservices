498b2025290f8cf105a40802d56a37ea2e59a30b
Roberto Fasciolo <rob@robyf.net>
2020-08-28 12:08:01 +0300

    Using autoconfiguration

--- src/main/java/net/robyf/ms/scoring/ExceptionHandling.java
+++
@@ -1,8 +0,0 @@
-package net.robyf.ms.scoring;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.ProblemHandling;
-
-@ControllerAdvice
-public class ExceptionHandling implements ProblemHandling {
-}
