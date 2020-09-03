dc5361120ee80cdd89c9ee1a4bccbcc1de09eb32
Roberto Fasciolo <rob@robyf.net>
2020-09-03 09:27:08 +0300

    Using gradle plugin and partially stuff from autoconfigure

--- src/main/java/net/robyf/ms/frontend/Application.java
+++ src/main/java/net/robyf/ms/frontend/Application.java
@@ -1,43 +1,25 @@
 package net.robyf.ms.frontend;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
+import net.robyf.ms.autoconfigure.ExceptionHandling;
+import net.robyf.ms.autoconfigure.MicroserviceConfiguration;
+import net.robyf.ms.autoconfigure.swagger.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.zalando.problem.ProblemModule;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.ZonedDateTime;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableCircuitBreaker
 @EnableFeignClients
+@Import({
+        ExceptionHandling.class,
+        SwaggerConfiguration.class,
+        MicroserviceConfiguration.class
+})
 public class Application {
-
-    @Bean
-    @Primary
-    public ObjectMapper objectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper().registerModule(new ProblemModule().withStackTraces());
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(ZonedDateTime.class, new ZonedDateTimeDeserializer());
-        objectMapper.registerModule(module);
-
-        return objectMapper;
-    }
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
-    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
