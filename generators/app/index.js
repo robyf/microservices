40ef3c41976482c0bab11732f1dcea3bc18e0166
Roberto Fasciolo <rob@robyf.net>
2020-09-04 07:33:09 +0300

    Basic stuff (gradle, .git*) in place

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,79 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the impeccable ${chalk.red('generator-microservice')} generator!`)
-    );
-
+  async prompting() {
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: "input",
+        name: "name",
+        message: "Project name",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Project friendly name (for documentation)",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Project description (for documentation)",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "package",
+        message: "Base package",
+        default: "net.robyf.ms"
+      },
+      {
+        type: "input",
+        name: "starterVersion",
+        message: "Starter version",
+        default: "Akaa.SR1-SNAPSHOT"
       }
     ];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.props = await this.prompt(prompts);
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath(".gitattributes"),
+      this.destinationPath(".gitattributes")
+    );
+    this.fs.copy(
+      this.templatePath(".gitignore"),
+      this.destinationPath(".gitignore")
+    );
+    this.fs.copy(
+      this.templatePath("gradle/**"),
+      this.destinationPath("gradle")
+    );
+    this.fs.copy(this.templatePath("gradlew"), this.destinationPath("gradlew"));
+    this.fs.copy(
+      this.templatePath("gradlew.bat"),
+      this.destinationPath("gradlew.bat")
+    );
+    this.fs.copy(
+      this.templatePath("gradle.properties"),
+      this.destinationPath("gradle.properties")
+    );
+    this.fs.copyTpl(
+      this.templatePath("settings.gradle"),
+      this.destinationPath("settings.gradle"),
+      { name: this.props.name }
+    );
+    this.fs.copyTpl(
+      this.templatePath("build.gradle"),
+      this.destinationPath("build.gradle"),
+      { name: this.props.name, starterVersion: this.props.starterVersion }
     );
   }
 
   install() {
-    this.installDependencies();
+    // this.installDependencies();
   }
 };
