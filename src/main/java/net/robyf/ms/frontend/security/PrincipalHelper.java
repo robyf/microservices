0d19a3f796e781641b3cb0a0a21ff94d35631ba3
Roberto Fasciolo <rob@robyf.net>
2020-09-09 08:55:45 +0300

    Cleanup

--- src/main/java/net/robyf/ms/frontend/security/PrincipalHelper.java
+++ src/main/java/net/robyf/ms/frontend/security/PrincipalHelper.java
@@ -1,6 +1,5 @@
 package net.robyf.ms.frontend.security;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,23 +8,19 @@ import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 
 @Service
-@Slf4j
 public class PrincipalHelper {
 
     public Principal getPrincipal() {
-        log.info("getPrincipal");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth instanceof UsernamePasswordAuthenticationToken) {
-            return (Principal) ((UsernamePasswordAuthenticationToken) auth).getPrincipal();
+            return (Principal) auth.getPrincipal();
         }
         return null;
     }
 
     public Principal ensurePrincipal() {
-        log.info("ensurePrincipal");
         Principal principal = this.getPrincipal();
         if (principal == null) {
-            log.info("Null principal");
             throw Problem.valueOf(Status.UNAUTHORIZED);
         }
         return principal;
