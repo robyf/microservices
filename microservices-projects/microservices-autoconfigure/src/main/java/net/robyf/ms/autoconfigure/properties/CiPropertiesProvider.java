7200d9aaea52691adfb1511227e82e65e4d43586
Roberto Fasciolo <rob@robyf.net>
2020-09-09 11:19:01 +0300

    Timeouts

--- microservices-projects/microservices-autoconfigure/src/main/java/net/robyf/ms/autoconfigure/properties/CiPropertiesProvider.java
+++ microservices-projects/microservices-autoconfigure/src/main/java/net/robyf/ms/autoconfigure/properties/CiPropertiesProvider.java
@@ -17,6 +17,10 @@ class CiPropertiesProvider implements PropertiesProvider {
         props.put(PropertyNames.LOGGING_LEVEL_NET_ROBYF_MS, "INFO");
 
         props.put(PropertyNames.FEIGN_HYSTRIX_ENABLED, true);
+        props.put(PropertyNames.FEIGN_CLIENT_CONFIG_DEFAULT_CONNECT_TIMEOUT, 10000);
+        props.put(PropertyNames.FEIGN_CLIENT_CONFIG_DEFAULT_READ_TIMEOUT, 10000);
+
+        props.put(PropertyNames.HYSTRIX_COMMAND_DEFAULT_EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS, 10000);
 
         props.put(PropertyNames.INFO_APP_NAME, appName);
         props.put(PropertyNames.INFO_APP_VERSION, appVersion);
--- microservices-projects/microservices-autoconfigure/src/main/java/net/robyf/ms/autoconfigure/properties/PropertyNames.java
+++ microservices-projects/microservices-autoconfigure/src/main/java/net/robyf/ms/autoconfigure/properties/PropertyNames.java
@@ -5,6 +5,10 @@ abstract class PropertyNames {
     static final String LOGGING_LEVEL_NET_ROBYF_MS = "logging.level.net.robyf.ms";
 
     static final String FEIGN_HYSTRIX_ENABLED = "feign.hystrix.enabled";
+    static final String FEIGN_CLIENT_CONFIG_DEFAULT_CONNECT_TIMEOUT = "feign.client.config.default.connectTimeout";
+    static final String FEIGN_CLIENT_CONFIG_DEFAULT_READ_TIMEOUT = "feign.client.config.default.readTimeout";
+
+    static final String HYSTRIX_COMMAND_DEFAULT_EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS = "hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds";
 
     static final String INFO_APP_NAME = "info.app.name";
     static final String INFO_APP_VERSION = "info.app.version";
