ed5671fbd49f01348d0f732cde0f3688a8f624d3
Roberto Fasciolo <rob@robyf.net>
2020-09-11 09:54:40 +0300

    Using lombok for no args constructor

--- src/main/java/net/robyf/ms/frontend/security/Principal.java
+++ src/main/java/net/robyf/ms/frontend/security/Principal.java
@@ -1,22 +1,21 @@
 package net.robyf.ms.frontend.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.experimental.Tolerate;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Principal implements Serializable {
 
     private UUID userId;
     private UUID accountId;
     private UUID sessionId;
-
-    @Tolerate // NOSONAR
-    public Principal() { // NOSONAR
-    }
 
 }
